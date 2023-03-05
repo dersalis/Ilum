2d3cceede69a18387b180f20ff13dee657ab2fe7
Damian <hello@damianruta.pl>
2023-03-05 16:36:19 +0100

    Configurations

---
+++ Ilum.Api/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Ilum.Api.Configurations
+{
+	public static class AuthenticationConfiguration
+	{
+		public static void AddAuthenticationServices(this IServiceCollection services)
+		{
+			services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = ConfigurationManager.AppSetting["JWT:ValidIssuer"],
+                    ValidAudience = ConfigurationManager.AppSetting["JWT:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]))
+                };
+            });
+        }
+
+    }
+}
+
--- Ilum.Api/Program.cs
+++ Ilum.Api/Program.cs
@@ -10,69 +10,14 @@ using ConfigurationManager = Ilum.Api.Configurations.ConfigurationManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-//builder.Services.AddDbContext<IlumContext>(); // DbContext
 builder.Services.AddDbContextServices();
-
-builder.Services.AddMediatR(Assembly.GetEntryAssembly()); // MediatR
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // AutoMapper
-
+builder.Services.AddMediatRServices();
+builder.Services.AddAutoMapperServices();
+builder.Services.AddSwaggerServices();
+builder.Services.AddAutoMapperServices();
 builder.Services.AddScoped<IIlumContext>(provider => provider.GetService<IlumContext>());
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("V1", new OpenApiInfo
-    {
-        Version = "V1",
-        Title = "WebAPI",
-        Description = "Product WebAPI"
-    });
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Description = "Bearer Authentication with JWT Token",
-        Type = SecuritySchemeType.Http
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Id = "Bearer",
-                    Type = ReferenceType.SecurityScheme
-                }
-            },
-            new List<string>()
-        }
-    });
-});
-
-builder.Services.AddAuthentication(opt => {
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = ConfigurationManager.AppSetting["JWT:ValidIssuer"],
-            ValidAudience = ConfigurationManager.AppSetting["JWT:ValidAudience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]))
-        };
-    });
 
 var app = builder.Build();
 
